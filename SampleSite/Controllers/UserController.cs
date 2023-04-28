aa6dfbb20f71bcc4645570f5beeaf940f89a0edc
kadorek <haltaskadir@gmail.com>
2023-04-28 13:51:42 +0300

    user operations added

---
+++ SampleSite/Controllers/UserController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SampleSite.Models;
+
+namespace SampleSite.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly SampleContext _context;
+
+        public UserController(IConfiguration config)
+        {
+            _context = new SampleContext(config);
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return _context.Users != null ? 
+                          View(await _context.Users.ToListAsync()) :
+                          Problem("Entity set 'SampleContext.Users'  is null.");
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,UserName,Password,CreateDate,IsActive")] User user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'SampleContext.Users'  is null.");
+            }
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserExists(long id)
+        {
+          return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- SampleSite/Models/User.cs
+++ SampleSite/Models/User.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SampleSite.Models;
 
 public partial class User
 {
     public long Id { get; set; }
-
+    [Required(ErrorMessage ="Kullanıcı adı boş geçilemez.")]
+    [MaxLength(5,ErrorMessage ="Kullanıcı adı en fazla 5 karakter olabilir."),MinLength(2,ErrorMessage ="Kullanıcı adı minimum 2 karakter olabilir.")]    
     public string UserName { get; set; } = null!;
 
     public string Password { get; set; } = null!;
 
+    [NotMapped]
+    [Display(Name ="Confirm Password",Prompt =" Type password again.")]
+    [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor")]
+    public string ConfirmPassword { get; set; }
+
+    [DataType(DataType.Date)]
     public string CreateDate { get; set; } = null!;
 
     public long IsActive { get; set; }
